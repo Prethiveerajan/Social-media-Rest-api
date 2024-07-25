86a605f6af1eb5132c948416bb18621aef8ada71
Prethiveerajan05 <raj0105200@gmail.com>
2024-07-25 17:57:47 +0530

    added salting and bcrypt

--- controller/User.js
+++ controller/User.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const User = require('../models/userModel')
+const express = require("express");
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
-const get =  (req, res) => {
-    res.json({ message: 'Welcome to the API!' });
-}
+const get = (req, res) => {
+  res.json({ message: "Welcome to the API!" });
+};
 
-const GetUser = async (req,res) =>{
+const GetUser = async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(req.body.password, salt);
     const newUser = new User({
-        userName: req.body.userName,
-        email: req.body.email,
-        password: req.body.password
-    })
-    try{
-        const savedUser = await newUser.save()
-        res.status(200).json(savedUser)
-    }
-    catch(error){
-        res.status(400).json({message: error.message})
-    }
-}
-const DeleteUser = async (req,res)=>{
-    try{
-        const user = await User.findByIdAndDelete(req.params.id)
-        if(!user) return res.status(400).json({message: 'User not found'})
-        res.status(200).json({message: 'User deleted successfully'})
-    }
-    catch{
-        res.status(400).json({message:"user can't be deleted"})
-    }
+      userName: req.body.userName,
+      email: req.body.email,
+      password: hashedpassword,
+    });
 
-}
+    const savedUser = await newUser.save();
+    res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+const DeleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(400).json({ message: "User not found" });
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch {
+    res.status(400).json({ message: "user can't be deleted" });
+  }
+};
 
-module.exports = {get,GetUser,DeleteUser};
+module.exports = { get, GetUser, DeleteUser };
